d26c6457e136e74c13962526cba44d005ec2cd47
Cranky Bawler <6666lenia6666@mail.ru>
2024-06-08 11:28:10 +0300

    1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,16 @@
+import java.util.Random;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+
+    }
+    public static void random (){
+        int min = 0;
+        int max = 100;
+        int diff = max - min;
+        Random random = new Random();
+                int i = random.nextInt(diff+1);
+        i += min;
     }
 }
